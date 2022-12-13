4eed04770ce3f3972737fca3a6ef6ba847f11ce0
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-13 14:43:42 +0545

    promise api

--- javaScript-1/promise.js
+++ javaScript-1/promise.js
@@ -1,24 +1,28 @@
 let promise1 = new Promise((res, rej) => {
   setTimeout(() => {
     // console.log("done1");
-    res("value1");
+    rej("value1");
   }, 1000);
 });
 let promise2 = new Promise((res, rej) => {
   setTimeout(() => {
     // console.log("done2");
-    res("value3");
+    rej("value2");
   }, 2000);
 });
+
 let promise3 = new Promise((res, rej) => {
   setTimeout(() => {
     // console.log("done3");
-    res("value3");
+    rej("value3");
   }, 3000);
 });
 
-promise1.then(function () {
-  console.log(promise1);
-  console.log(promise2);
-  console.log(promise3);
-});
+// promise1.then(function () {
+//   console.log(promise1);
+//   console.log(promise2);
+//   console.log(promise3);
+// });
+
+let allPromises = Promise.any([promise1, promise2, promise3]);
+allPromises.then(console.log);
