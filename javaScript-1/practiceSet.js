cd53174dcd1d848e26edc01265fd913fc740132e
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-21 23:45:11 +0545

    commit

--- javaScript-1/practiceSet.js
+++ javaScript-1/practiceSet.js
@@ -1,1 +1,22 @@
-// // question no 1
+// question no 1
+
+//
+
+//
+
+// valueAdded = spt.setAttribute(
+//   "src",
+//   "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+// );
+
+async function loadFile() {
+  let promise1 = new Promise((res, rej) => {
+    let script = document.createElement("script");
+    document.body.append(script);
+    script.src =
+      "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js";
+    res(script);
+  });
+}
+
+loadFile().then(alert("It has been loaded"));
